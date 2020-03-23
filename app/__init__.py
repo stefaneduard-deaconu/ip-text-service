f733580d1056940b3f1b900918e8bb05e08b00bf
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-03-23 07:23:50 +0200

    switcher to pymongo 3.9.0, added connection to the local 'test' MongoDB

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,23 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Database"
+app.config["MONGO_URI"] = "mongodb://localhost:27071/test"
 mongo = PyMongo(app)
+
+
+#
+#
+# connection_params = {
+#     'user': 'stefan',
+#     'password': 'EKfBL0RgrrBnSkfB',
+#     'host': 'ip-skb3j.mongodb.net',
+#     'port': 25058,
+#     'namespace': 'test',
+# }
+# connection = MongoClient(
+#     'mongodb://{user}:{password}@{host}:'
+#     '{port}/{namespace}'.format(**connection_params)
+# )
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import mongo
 @app.route('/')
 @app.route('/index')
 def index():
-    return '''
+    return f'''
     <h1> Welcome to the text service <h1>
+    <div>{mongo.db.list_collection_names()}</div>
     '''
