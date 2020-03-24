718821aa5a0222a9299d6e160dcdfb2bc5729a2c
Marius Laptop <c_marian98@yahoo.com>
2020-03-25 01:48:14 +0200

    Added some todo's for ocr

--- app/ocr/routes.py
+++ app/ocr/routes.py
@@ -5,6 +5,11 @@ from app.ocr import translate
 # here we only keep the routes for the ocr, so we need to import all the functions from ocr
 @app.route('/ocr', methods=['GET'])
 def ocr():
+
+    #TODO: load image into memory, skip if it's passed as argument
+    #TODO: apply some transformation on image for better ocr result
+    #TODO: call tessaract function, return result
+
     translation = translate('This is where OCR magic happens.')
     return f"""
     <h1>This is where OCR magic happens.</h1>
