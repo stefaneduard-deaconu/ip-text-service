a8a656f07119383d6029a9360b700d088b85ed85
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-03-24 16:42:08 +0200

    ready for deployment using heroku envi var MONGO_URI

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 import os
+
 if os.path.isfile('mongo_uri.env'):
     from configparser import ConfigParser
     parser = ConfigParser()
     parser.read('mongo_uri.env')
     mongo = PyMongo(app, uri=parser['Mongo']['MONGO_URI'])
 else:
-    mongo = PyMongo(app)
+    mongo = PyMongo(app, uri=os.environ["MONGO_URI"])
 # import the routes for the flask application
 from app import routes
 
